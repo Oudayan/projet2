23846a5390fd835e529d5cdd2e3672634b26e036
zores <zorayda.ortiz@gmail.com>
2018-02-14 11:44:33 -0500

    footer css

--- vues/piedPage.php
+++ vues/piedPage.php
@@ -1,27 +1,70 @@
-            <div class="row">
-                <div class="col-sm-9 offset-sm-2 text-danger">
-                    <?php if (isset($data["errors"])) {
-                        if ($data["errors"] != "") {
-                            echo "<p>" . $data["errors"] . "</p>";
-                        }
-                    } ?>
-                </div>
-            </div>
-        </main> <!-- From Header.php -->
 
-        <footer class="container-fluid text-center bg-inverse text-white">
-            <div class="social">
-                <a href="#" class="fa fa-facebook"></a>
-                <a href="#" class="fa fa-twitter"></a>
-                <a href="#" class="fa fa-google"></a>
-                <a href="#" class="fa fa-youtube"></a>
-                <a href="#" class="fa fa-instagram"></a>
-                <a href="#" class="fa fa-pinterest"></a>
-            </div>
-            <?php
-            
-            ?>
+        <div class="d-flex justify-content-around" id="info">
+          <div class="circle">
+            <div class="row justify-content-center text-center">
+              <div class="col-8 col-sm-6">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </div>            
+            <h6>kkk hhfhfhfghfhd hfh</h6>
+            <p>fsdfs rthrtyhf  hjfhrgh  hfghrr  ryrt yr rtrtyrtr rty dfs s</p>
+          </div>
+        </div><!-- circle -->   
+          <div class="circle">
+            <div class="row justify-content-center text-center">
+              <div class="col-8 col-sm-6">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </div>            
+            <h6>kkk hhfhfhfghfhd hfh</h6>
+            <p>fsdfs rthrtyhf  hjfhrgh  hfghrr  ryrt yr rtrtyrtr rty dfs s</p>
+          </div>
+        </div><!-- circle --> 
+           <div class="circle">
+            <div class="row justify-content-center text-center">
+              <div class="col-8 col-sm-6">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </div>            
+            <h6>kkk hhfhfhfghfhd hfh</h6>
+            <p>fsdfs rthrtyhf  hjfhrgh  hfghrr  ryrt yr rtrtyrtr rty dfs s</p>
+          </div>
+        </div><!-- circle --> 
+           <div class="circle">
+            <div class="row justify-content-center text-center">
+              <div class="col-8 col-sm-6">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </div>            
+            <h6>kkk hhfhfhfghfhd hfh</h6>
+            <p>fsdfs rthrtyhf  hjfhrgh  hfghrr  ryrt yr rtrtyrtr rty dfs s</p>
+          </div>
+        </div><!-- circle --> 
+      
+        </div><!-- d-flex --> 
+
+<footer class="text-white"> 
+  
+        <div class="row mx-auto">     
+        <div class="col-md-4  justify-content-center">
+          <p>Service Client<br>
+          Aide aux locataires<br>
+          Nous contacter</p>
+        </div>  
+          <div class="col-md-4">
+          <p>Information sur la societé<br>
+          Contions générales<br>
+          Politiques de confidentialité</p>
+        </div> 
+        <div class="col-md-4">
+          <p class="text-center">nous somme social</p>
+          <div class="social d-flex justify-content-around">
+                <a href="https://fr-ca.facebook.com/" class="fa fa-facebook"></a>
+                <a href="https://plus.google.com" class="fa fa-google-plus"></a>
+                <a href="https://twitter.com" class="fa fa-twitter"></a>
+          </div>
+          </div>
+  </div>   
+      
+ 
         </footer>
+
 
         <script src="js/jquery.slim.min.js"></script>
         <script src="js/tether.min.js"></script>
