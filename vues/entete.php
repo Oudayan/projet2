f366d2bc9728e0bef25342e7bffe4f7980657690
zores <zorayda.ortiz@gmail.com>
2018-02-20 16:51:28 -0500

    fev 20 4h51

--- vues/entete.php
+++ vues/entete.php
@@ -75,9 +75,9 @@
                     </div><!-- ml-auto -->
 				   <?php }
 					else { ?>
-					<div><span>Usager: <strong></strong></span>						
+					<div><span>Usager: <strong><?= $_SESSION["prenom"];?></strong></span>						
                       <a href="index.php?Logement&action=formAjoutLogement">Ajouter un logement</a>
-                      <a href="index.php?Usagers&action=nouvelMessage"><i class="fa fa-envelope iconMessage"></i><?= $_SESSION["prenom"];?></a>
+                      <a href="index.php?Usagers&action=afficherMessagerie"><i class="fa fa-envelope iconMessage"></i><?= $_SESSION["courriel"];?></a>
                       <a href="index.php?Usagers&action=Logout" id="myBtn" class="nav-item nav-link" aria-hidden="true"><li class="fa fa-sign-out fa-lg bg-login"></li>Déconnexion</a>
 					</div>
 					<?php } ?>					
