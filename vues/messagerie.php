ce47e527c7947a8f1ba2872a0f2e6a5ffe343ad4
zores <zorayda.ortiz@gmail.com>
2018-02-21 22:14:58 -0500

    tables boiteReception et message Envoyes

--- vues/messagerie.php
+++ vues/messagerie.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @file     messgerie.php
+ * @file     messagerie.php
  * @author   Zoraida Ortiz
  * @version  2.0
  * @date     20 fevrier 2018
@@ -14,8 +14,11 @@
     <link rel="stylesheet" href="css/site.css">
     <link rel="stylesheet" href="css/richtext.scss">        
     <link rel="stylesheet" href="css/richtext.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+       <!-- <link rel="stylesheet" href="css/jquery.dataTables.min.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+   <!-- <script src="js/jquery.dataTables.min.js"></script> --> 
     <script src="js/jquery.richtext.js"></script>
+   <!-- <script src="js/messagerie.js"></script>-->
 </head>     
 
 <div class="container">
@@ -24,7 +27,7 @@
       <a class="nav-link" id="v-pills-compMessage-tab list-group-item-action" data-toggle="pill" href="#v-pills-compMessage" role="tab" aria-controls="v-pills-compMessage" aria-selected="true"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Composer un message</a>
        <div class="dropdown-divider"></div>
       <a class="nav-link" id="v-pills-boitRecp-tab list-group-item-action" data-toggle="pill" href="#v-pills-boitRecp" role="tab" aria-controls="v-pills-boitRecp" aria-selected="false"><span class="badge badge-default badge-pill" aria-hidden="false">14</span><i class="fa fa-folder-open" aria-hidden="true"></i>Boîte de réception</a>      
-      <a class="nav-link" id="v-pills-mEnvoyes-tab list-group-item-action" data-toggle="pill" href="#v-pills-mEnvoyes" role="tab" aria-controls="v-pills-mEnvoyes" aria-selected="false"><i class="fa fa-paper-plane" aria-hidden="true"></i>Messages envoyés</a>
+      <a class="nav-link" id="v-pills-mEnvoyes-tab list-group-item-action" onclick="afficherMsgEnvoyes()" data-toggle="pill" href="#v-pills-mEnvoyes" role="tab" aria-controls="v-pills-mEnvoyes" aria-selected="false"><i class="fa fa-paper-plane" aria-hidden="true"></i>Messages envoyés</a>
     </div><!-- nav flex-column -->
 
     <div class="tab-content tab-messagerie col-9" id="v-pills-tabContent">
@@ -62,33 +65,83 @@
       </div><!-- tab-pane compMessage -->
       
       <div class="tab-pane fade show active" id="v-pills-boitRecp" role="tabpanel" aria-labelledby="v-pills-boitRecp-tab">
-        <table class="table table-sm responsive-sm table-hover">
+        <table class="table table-sm responsive-sm table-hover display" id="messages">
           <thead>
             <tr>
-              <th scope="col">#</th>
+              <th scope="col">lu</th>
+              <th><i class="fa fa-level-down" aria-hidden="true"></i></th>
+              <th>Sujet</th>
+              <th><i class="fa fa-paperclip" aria-hidden="true"></i></th>
+              <th><i class="fa fa-calendar-plus-o" aria-hidden="true"></i></th>
             </tr>
           </thead>
-          <tbody>
-            <tr onclick="afficherMessage(this)">
-              <td>
-               <i class="fa fa-envelope" aria-hidden="true"></i>
-              </td>
-              <td>pepito perez</td>
-              <td><a href="">demo messagerie</a></td>
-              <td><a href=""><i class="fa fa-paperclip" aria-hidden="true"></i></a></td>
-              <td>17 fev 2017</td>
-            </tr>            
+          <tbody id="boiteReception">
           </tbody>
         </table>
       </div><!-- tab-pane boitRecp-->
       
-      <div class="tab-pane fade" id="v-pills-mEnvoyes" role="tabpanel" aria-labelledby="v-pills-mEnvoyes-tab"> gdgd dfd  dfgsd...</div>
-      </div><!-- tab-pane -->
+      <div class="tab-pane fade" id="v-pills-mEnvoyes" role="tabpanel" aria-labelledby="v-pills-mEnvoyes-tab">
+        <table class="table table-sm responsive-sm table-hover display">
+          <thead>
+            <tr>
+              <th scope="col">lu</th>
+              <th><i class="fa fa-level-up" aria-hidden="true"></i></th>
+              <th>Sujet</th>
+              <th><i class="fa fa-paperclip" aria-hidden="true"></i></th>
+              <th><i class="fa fa-calendar-plus-o" aria-hidden="true"></i></th>
+            </tr>
+          </thead>
+          <tbody id="msgEnvoyes">
+          </tbody>
+        </table>
+      </div>        
+    </div><!-- tab-pane -->
     </div><!-- tab-messagerie -->
   </div><!-- d-flex flex-row -->
 </div><!-- container -->
- <script>
-        $(document).ready(function() {
-            $('.content').richText();
+<script type="text/javascript">
+  $(document).ready(function() {
+        $('.content').richText(); 
+        afficherBoiteReception();
+    });
+        
+</script>
+ <script type="text/javascript">
+    function afficherBoiteReception() {
+    $.ajax({
+        url: 'index.php?Messagerie&action=boiteReception', 
+        type: 'POST',
+        dataType: 'json',
+       success: function(json) {
+        $.each(json, function(i, item) {
+           $("#boiteReception").append("<tr><td>" + item.lu + "</td><td>" + item.nom + "</td><td>" + item.sujet + "</td><td>" + item.fichier_joint + "</td><td>" + item.msg_date + "</td></tr>");
+
+          });
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+            }
+    });
+};
+        
+</script>
+ <script type="text/javascript">
+    function afficherMsgEnvoyes() {
+        $.ajax({
+            url: 'index.php?Messagerie&action=msgEnvoyes', 
+            type: 'POST',
+            dataType: 'json',
+           success: function(json) {
+            $.each(json, function(i, item) {
+               $("#msgEnvoyes").append("<tr><td>" + item.lu + "</td><td>" + item.nom + "</td><td>" + item.sujet + "</td><td>" + item.fichier_joint + "</td><td>" + item.msg_date + "</td></tr>");
+         
+              });
+				},
+				error: function(xhr, ajaxOptions, thrownError) {
+					alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+				}
         });
-        </script>
+    }; 
+ </script>
+        
+ 
