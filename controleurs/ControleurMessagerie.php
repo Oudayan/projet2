e1562735862c5ecd69c61f7c2dffd7211c5d20c9
Ratté <e1795137@b1128-03.local>
2018-02-28 12:10:56 -0500

    Changements controleurMessagerie

--- controleurs/ControleurMessagerie.php
+++ controleurs/ControleurMessagerie.php
@@ -35,7 +35,7 @@
 						// aller chercher les messages recues
                       
                     
-                    case "chercherMessages":  
+                    case "messagesRecus":  
                         $modeleMessagesDestinataires = $this->getDao("MessagesDestinataires");
                         $recus = $modeleMessagesDestinataires->obtenir_par_expediteur($_SESSION["courriel"])
                                
@@ -46,191 +46,34 @@
                                 $donnees[$i][1]=this->lireId_message();
                                 $donnees[$i][2]=this->lireId_reference();
                                 $donnees[$i][3]=this->lireSujet();
-                                $donnees[$i][4]=this->lireFichier_joint()();
+                                $donnees[$i][4]=this->lireFichier_joint();
+                                $donnees[$i][4]=this->lireLu();   
                                 $donnees[$i][5]=this->lireExpediteur();
-                        }
-                            
-                        
-							
-							echo json_encode($recus);
-							return;					//affiche la liste des messages recus
+                        	}  
+							echo json_encode($donnees);
+							return;					                                                 //contient la liste des messages recus
 							break;  
                         
-                        
-                        
-                        
-                        
-                        
-                        
                         	
-																									
-		
-								$modeleUsagers = $this->lireDao("Usagers");
-								//$nouveauUsager = new Usagers();
-								//$nouveauUsager->ecrireCourriel($params["courriel"]);
-								//$my=$nouveauUsager->lireCourriel();
-								// var_dump("my",$modeleUsagers);
-								$data = $modeleUsagers->obtenir_par_courriel($params["courriel"]);  
-								//var_dump($data);
-								if($data && $data->lireCourriel() == $params["courriel"] && $data->lireMotDePasse() == $params["MotDePasse"])	//verifie si $data est "vraie" et si les donnees de la bd sont pareil comme les entrées.
-								{																								//$data sera faux si le courriel ne se trouve pas dans la bd
-																		
-									if($data->lireestBanni() == 1)										//si l'usager est bannis
-									{
-										$this->afficherVues("MessageBanned");							//message pour les usagers bannis du site et destruction de la session
-										if (isset($_SESSION["courriel"]))
-											session_destroy();																	
-									}	
-									else															
-									{	
-			
-										 
-										// $controleur = "Sujets"; 									// chercher la classe avec le nom du controleur Sujets pour pouvoir afficher la liste des sujets
-										// $classe = "Controleur_" . $controleur;
-										//if(class_exists($classe))
-										/*{
 
-											$objetControleur = new $classe;							
-											if($objetControleur instanceof BaseControleur)
-											{
-												$_REQUEST["action"] = "afficheListeSujets";
-												$objetControleur->traite($_REQUEST);
-										 	}
-											else
-											trigger_error("Controleur invalide.");
-										} */
-									}
-									$_SESSION["courriel"] = $params["courriel"];	
-									$_SESSION["typeUser"] = $data->lireTypeUsager();
-								    $_SESSION["alert"]= "Reussi";
-									header("Location: index.php");
-							
-								}
-								else
-								{
-									var_dump("Le courriel ou le MotDePasse est inexact");   
-									$_SESSION["warning"]= "Le courriel ou le MotDePasse est inexact";
-									header("Location: index.php");
-								
-								}
-
-						}
-						else {
-							var_dump("Erreur en parametres");
-						    die();
-						}
-						
-					break;
-					
-					case "ajouterUsager" : 
-						$modeleTypeContact = $this->lireDAO("TypeContact");
-						$modeleTypePaiement = $this->lireDAO("TypePaiement");
-					  	$donnees["listeContacts"] = $modeleTypeContact->lireTousTypeContact();
-					  	$donnees["listePaiements"] = $modeleTypePaiement->lireTousTypePaiement();
-					  	$this->afficherVues("ajoutUsager", $donnees);
-					break;
-
-					//====================================================partie administrative===========================
-					
-				
-					case "afficheListeUsagers":														//affiche la liste des usagers
-						$this->afficheListeUsagers();
-					break;					
-									
-					
-					case "modifieUsager":															//va chercher un usager pour permettre la modification
-						
-						if(isset($params["courriel"]))
-						{
-							$modeleUsagers = $this->lireDAO("Usagers");
-							$data = $modeleUsagers->obtenir_par_courriel($params["courriel"]);		//obtenir les informations d'un usager en se servant du courriel de AfficheListeUsagers
-							$this->afficherVues("AfficheUsager", $data);								//affiche une vue de l'usager que l'on veut modifier
-						}
-						
-						else
-						{
-							trigger_error("Pas de courriel spécifié...");
-						}
-						break;
 					
 					
-					case "enregistrerUsager" :														//va chercher un usager pour permettre la modification
-						var_dump($params);
-						//die();
-						if(!isset($params["courriel"]) || !isset($params["nom"]) || !isset($params["prenom"]))
-						{	
-					       echo "Erreur .... !";
-							$this->afficherVues("AfficheUsager");
-						}
-						else
-						{
-								$modeleUsagers = $this->lireDAO("Usagers");                                  
-								$modification["Usager"] = new Usagers($params["courriel"],$params["nom"],$params["prenom"], $params["mot_de_passe"], $params["cellulaire"],"","","",$params["id_contact"],2,$params["id_paiement"]);
-								$succes= $modeleUsagers->sauvegarde($modification["Usager"]);		//sauvegarder les informations d'un usager en se servant d'un tableau
-								$this->afficheListeUsagers();
-						}
-						break;
-					case "Logout":																	//va chercher un usager pour permettre la modification
-					   $this->deconnection();
-					   header("Location: index.php");
-
-					break;
-                    
-                    case "nouvelMessage":
-                      $this->afficherVues("messagerie");
 					/*default:		
 																								
 						trigger_error("Action invalide");
 					*/	
-				}	
-			}
+				}                                                                                   // fin du switch	
+			}                                                                                       //fin du if params action
 			else
 			{
-				var_dump("No");
-				$this->afficherVuess("FormLogin"); 													//action par defaut- affiche le login
-			}	
-		}
+				//var_dump("No");
+				$this->afficherVues("messagerie"); 													//action par defaut- affiche la page d'accueil de la messagerie
+			}                                                                                       // fin du else du param action	
+		}                                                                                           //fin de la fonction index
 		
-		/**
-		* @brief Affiche la liste des usagers
-		* @details Prend les renseignements sur les usagers et les affiche. Puis ouvre la vue AfficheListeUsagers
-		* @details Utilise le Modele_Usagers
-		* @param point1 data
-		* @return à une vue.
-		*/
-
-		private function afficheListeUsagers()
-		{
-			$modeleUsagers = $this->lireDAO("Usagers");
-			$data["usagers"] = $modeleUsagers->obtenir_tous();
-			$this->afficherVues("AfficheListeUsagers", $data);
-		}
-
 		
-		/**
-		* @brief Permet la déconnection de la session
-		* @details Se retouve sur les pages ecepté sur la page de loggin
-		* @param point1 courriel
-		* @return aucun.
-		*/
-		private function deconnection()
-		{
 		
-			if (isset($_SESSION["courriel"])) 
-			{
-				unset($_SESSION["courriel"]);
-				setcookie("courriel", null, -1, '/');
-			}
-			if (isset($_SESSION["typeUser"]))
-			{
-				unset($_SESSION["typeUser"]);
-				setcookie("typeUser", null, -1, '/');
-
-			}	
-
-		}
-		
-	}
+	}                                                                                               //fin de la classe ControleurMessagerie
 	
 		
 ?>
