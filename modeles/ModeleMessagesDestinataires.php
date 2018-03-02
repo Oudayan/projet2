42e13a94e3fdeb46c961881cf61e9fff584e7a3a
Ratté <e1795137@b1128-03.local>
2018-03-02 15:09:18 -0500

    modifier le fichier

--- modeles/ModeleMessagesDestinataires.php
+++ modeles/ModeleMessagesDestinataires.php
@@ -65,6 +65,85 @@
           
         }
         
+        /**
+		* @brief Pour aller chercher un message
+		* @details Permet d'aller chercher les renseignements sur un message reçu en utilisant le id du message.
+		* @param point1 id_message
+		* @param point2 resultat
+		* @return array unMessage.
+		*/
+		public function obtenir_par_id_message($id_message)
+		{
+			$resultat = $this->lire($id_message);//reference BaseDAO
+			$resultat->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, '"MessagesDestinataires"'); 
+			$unMessage = $resultat->fetch();
+			return $unMessage;
+		}
+        
+         /**
+		* @brief Pour aller chercher un message
+		* @details Permet d'aller chercher les renseignements sur un message reçu en utilisant le courriel de l'utilisateur.
+		* @param point1 expediteur
+		* @param point2 resultat
+		* @return array unMessage.
+		*/
+		public function obtenir_par_expediteur($expediteur)
+		{
+			$resultat = $this->lire($expediteur, "expediteur");//reference BaseDAO
+			$resultat->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Messages'); 
+			$unMessage = $resultat->fetch();
+			return $unMessage;
+		}
+		
+        
+        /**
+		* @brief Sauvegarde un message
+		* @details Prend les informations entrées et les sauvegarde dans la base de données.
+		* @param point1 unMessage
+		* @param point2 id_message
+		* @param point3 sujet
+		* @param point4 fichier_joint
+		* @param point5 message
+		* @param point6 msg_date
+		* @param point7 courriel
+		* @return aucun.
+		*/
+		/**
+        public function sauvegarde(Usagers $unUsager)
+		{
+
+		/*	if($unUsager->courriel && $this->lire($unUsager->courriel)->fetch())
+			{
+				$query = "UPDATE " . $this->getTableName() . " SET nom=?, prenom=?, isAdmin=?, isBanned=? WHERE courriel = ?";
+				$donnees = array($unUsager->nom,$unUsager->prenom,$unUsager->isAdmin,$unUsager->isBanned,$unUsager->courriel) ;
+				$resultat = $this->requete($query, $donnees);
+			
+			}
+			
+			
+			
+			else
+			{ */
+				//insert
+/**
+				//var_dump($unMessage);
+				$query = "INSERT INTO " . $this->checherNomTable() . "(sujet, fichier_joint, message, msg_date, courriel,) VALUES (?, ?, ?, ?, ?)";
+				$donnees = array($unMessage->lireSujet(), $unMessage->lireFichier_joint(),	$unMessage->lireMessage(),
+				$unMessage->lireMsg_date(),$unMessage->lireCourriel(),
+				$unMessage->lireContact(),$unMessage->lireTypeUsager(),	$unMessage->lireTypePaiement()
+
+				//var_dump($unUsager);
+				$query = "INSERT INTO " . $this->lireNomTable() . "(courriel, nom, prenom, cellulaire, mot_de_passe, id_contact, id_type_usager, id_paiement) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+				$donnees = array($unUsager->lireCourriel(), $unUsager->lireNom(),	$unUsager->lirepreNom(),
+				$unUsager->lireCellulaire(),$unUsager->lireMotDePasse(),
+				$unUsager->lireContact(),$unUsager->lireTypeUsager(),	$unUsager->lireTypePaiement()
+
+				);
+
+				return $this->requete($query, $donnees);
+			/*}*/
+
+	
         
 	} //fin de la class ModeleMessagesDestinataires 
 		
